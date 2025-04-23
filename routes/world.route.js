b04d24f87a7229edbddd51949b238d506244d470
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-23 17:02:19 +0700

    update

--- routes/world.route.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { getWorlds } = require('../controllers/world.controller');
-
-router.get('/', getWorlds);
-
-module.exports = router;
---
+++ routes/world.routes.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const router = express.Router();
+const World = require('../models/world.model');
+const Npc = require('../models/npc.model');
+
+router.get('/', async (req, res) => {
+  try {
+    const worlds = await World.find({}).lean();
+
+    // Populate ruler thông qua npc model
+    for (const world of worlds) {
+      for (const region of world.regions) {
+        if (region.rulerId) {
+          const ruler = await Npc.findById(region.rulerId).select('name avatar currentRole');
+          region.ruler = ruler;
+        }
+
+        for (const city of region.cities) {
+          if (city.rulerId) {
+            const ruler = await Npc.findById(city.rulerId).select('name avatar currentRole');
+            city.ruler = ruler;
+          }
+        }
+      }
+    }
+
+    res.json(worlds);
+  } catch (err) {
+    res.status(500).json({ message: 'Lỗi khi lấy dữ liệu thế giới', error: err.message });
+  }
+});
+
+module.exports = router;
