15a5b40e5a370ec7912e24df7b5d165169cfe32c
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-29 15:27:37 +0700

    up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,6 @@ exports.register = async (req, res) => {
     const existingUser = await User.findOne({ username });
     if (existingUser) return res.status(400).json({ message: 'Username đã tồn tại' });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ username, password });
     res.status(201).json({ message: 'Đăng ký thành công', user });
   } catch (err) {
