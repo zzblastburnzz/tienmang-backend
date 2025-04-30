b63581bde1af2ff5fd359190e25803b34b6365c5
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-30 22:14:21 +0700

    up

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -1,12 +1,12 @@
 // src/controllers/user.controller.js
 const asyncHandler = require('express-async-handler');
-const User = require('../models/character.model');
+const Character = require('../models/character.model');
 
 // @desc    Get character profile
 // @route   GET /api/character/profile
 // @access  Private
 const getCharacterProfile = asyncHandler(async (req, res) => {
-  const user = await Character.findById(req.character._id);
+  const character = await Character.findById(req.character._id);
   if (character) {
     res.json(character);
   } else {
@@ -19,35 +19,35 @@ const getCharacterProfile = asyncHandler(async (req, res) => {
 // @route   PUT /api/character/updateCharacterProfile
 // @access  Private
 const updateCharacterProfile = asyncHandler(async (req, res) => {
-  const user = await Character.findById(req.character._id);
+  const character = await Character.findById(req.character._id);
 
   if (character) {
     const { displayName, gender, location } = req.body;
 
-    if (displayName) user.displayName = displayName;
-    if (gender) user.gender = gender;
-    if (location) user.location = location;
-    if (avatar) user.avatar = avatar;
-    if (cover) user.cover = cover;
+    if (displayName) character.displayName = displayName;
+    if (gender) character.gender = gender;
+    if (location) character.location = location;
+    if (avatar) character.avatar = avatar;
+    if (cover) character.cover = cover;
 
     // Cập nhật các chỉ số xã hội nếu gửi lên
-    if (friendshipScore !== undefined) user.friendshipScore = friendshipScore;
-    if (charisma !== undefined) user.charisma = charisma;
-    if (loveScore !== undefined) user.loveScore = loveScore;
-    if (trustScore !== undefined) user.trustScore = trustScore;
-    if (responsibility !== undefined) user.responsibility = responsibility;
-    if (ambition !== undefined) user.ambition = ambition;
-    if (leadershipSkill !== undefined) user.leadershipSkill = leadershipSkill;
-    if (fame !== undefined) user.fame = fame;
-    if (craftingSkill !== undefined) user.craftingSkill = craftingSkill;
+    if (friendshipScore !== undefined) character.friendshipScore = friendshipScore;
+    if (charisma !== undefined) character.charisma = charisma;
+    if (loveScore !== undefined) character.loveScore = loveScore;
+    if (trustScore !== undefined) character.trustScore = trustScore;
+    if (responsibility !== undefined) character.responsibility = responsibility;
+    if (ambition !== undefined) character.ambition = ambition;
+    if (leadershipSkill !== undefined) character.leadershipSkill = leadershipSkill;
+    if (fame !== undefined) character.fame = fame;
+    if (craftingSkill !== undefined) character.craftingSkill = craftingSkill;
 
     // Kiểm tra kích hoạt Dynamic Hidden Attributes
-    if (user.friendshipScore > 50 && user.charisma > 60) user.dynamicHiddenAttributes.loveActivated = true;
-    if (user.loveScore > 70 && user.trustScore > 50) user.dynamicHiddenAttributes.marriageStatus = 'Engaged';
-    if (user.loveScore > 80 && user.dynamicHiddenAttributes.marriageStatus === 'Married') user.dynamicHiddenAttributes.hasChildren = true;
-    if (user.responsibility > 60 && user.ambition > 70) user.dynamicHiddenAttributes.careerPath = 'Quan chức';
-    if (user.leadershipSkill > 80 && user.fame > 100) user.dynamicHiddenAttributes.clanLeaderPotential = true;
-    if (user.craftingSkill > 60 && user.responsibility > 50) user.dynamicHiddenAttributes.farmOwner = true;
+    if (character.friendshipScore > 50 && character.charisma > 60) character.dynamicHiddenAttributes.loveActivated = true;
+    if (character.loveScore > 70 && character.trustScore > 50) character.dynamicHiddenAttributes.marriageStatus = 'Engaged';
+    if (character.loveScore > 80 && character.dynamicHiddenAttributes.marriageStatus === 'Married') character.dynamicHiddenAttributes.hasChildren = true;
+    if (character.responsibility > 60 && character.ambition > 70) character.dynamicHiddenAttributes.careerPath = 'Quan chức';
+    if (character.leadershipSkill > 80 && character.fame > 100) character.dynamicHiddenAttributes.clanLeaderPotential = true;
+    if (character.craftingSkill > 60 && character.responsibility > 50) character.dynamicHiddenAttributes.farmOwner = true;
 
     const updatedCharacter = await character.save();
 
