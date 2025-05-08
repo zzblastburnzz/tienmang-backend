0f569adc987d9ee64be99db40b9adb5c3a931d8f
Ant <thanhpc.luxuryhome@gmail.com>
2025-05-08 15:31:47 +0700

    Revert "Merge branch 'main' of https://github.com/zzblastburnzz/tienmang-backend"
    This reverts commit ea991045174cc51660860fc131f32856b6a3db1c, reversing
    changes made to 9d4d039d4da37d18ca63d7dc4874570ce86fb2f5.

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -1,63 +1,96 @@
-
 const asyncHandler = require('express-async-handler');
 const Character = require('../models/character.model');
 
-// GET /character/profile
+// @desc    Get character profile
+// @route   GET /character/profile
+// @access  Private
 const getCharacterProfile = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const character = await Character.findOne({ userId });
-  if (!character) {
+  const character = await Character.findOne({ userId: req.user._id });
+  if (character) {
+    const profileComplete = !!(character.displayName && character.gender);
+    res.json({ ...character.toObject(), profileComplete });
+  } else {
     res.status(404);
     throw new Error('Character not found');
   }
-  res.json(character);
 });
 
-// PATCH /character/profile
+// @desc    Update or create character profile
+// @route   PUT /character/updateProfile
+// @access  Private
 const updateCharacterProfile = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const updates = req.body;
-  const character = await Character.findOneAndUpdate({ userId }, updates, { new: true });
+  let character = await Character.findOne({ userId: req.user._id });
+  const {
+    displayName,
+    gender,
+    location,
+    avatar,
+    cover,
+    friendshipScore,
+    charisma,
+    loveScore,
+    trustScore,
+    responsibility,
+    ambition,
+    leadershipSkill,
+    fame,
+    craftingSkill
+  } = req.body;
+
   if (!character) {
-    res.status(404);
-    throw new Error('Character not found');
+    if (!displayName || !gender || !location) {
+      res.status(400);
+      throw new Error('Thiếu thông tin để tạo nhân vật');
+    }
+    character = await Character.create({
+      userId: req.user._id,
+      displayName,
+      gender,
+      location,
+      avatar,
+      cover,
+      dynamicHiddenAttributes: {}
+    });
+    return res.status(201).json(character);
   }
-  res.json(character);
+
+  if (displayName) character.displayName = displayName;
+  if (gender) character.gender = gender;
+  if (location) character.location = location;
+  if (avatar) character.avatar = avatar;
+  if (cover) character.cover = cover;
+
+  if (friendshipScore !== undefined) character.friendshipScore = friendshipScore;
+  if (charisma !== undefined) character.charisma = charisma;
+  if (loveScore !== undefined) character.loveScore = loveScore;
+  if (trustScore !== undefined) character.trustScore = trustScore;
+  if (responsibility !== undefined) character.responsibility = responsibility;
+  if (ambition !== undefined) character.ambition = ambition;
+  if (leadershipSkill !== undefined) character.leadershipSkill = leadershipSkill;
+  if (fame !== undefined) character.fame = fame;
+  if (craftingSkill !== undefined) character.craftingSkill = craftingSkill;
+
+  if (character.friendshipScore > 50 && character.charisma > 60) {
+    character.dynamicHiddenAttributes.loveActivated = true;
+  }
+  if (character.loveScore > 70 && character.trustScore > 50) {
+    character.dynamicHiddenAttributes.marriageStatus = 'Engaged';
+  }
+  if (character.loveScore > 80 && character.dynamicHiddenAttributes.marriageStatus === 'Married') {
+    character.dynamicHiddenAttributes.hasChildren = true;
+  }
+  if (character.responsibility > 60 && character.ambition > 70) {
+    character.dynamicHiddenAttributes.careerPath = 'Quan chức';
+  }
+  if (character.leadershipSkill > 80 && character.fame > 100) {
+    character.dynamicHiddenAttributes.clanLeaderPotential = true;
+  }
+  if (character.craftingSkill > 60 && character.responsibility > 50) {
+    character.dynamicHiddenAttributes.farmOwner = true;
+  }
+
+  const updatedCharacter = await character.save();
+  res.json(updatedCharacter);
 });
 
-// POST /character/render-image
-const renderCharacterImage = asyncHandler(async (req, res) => {
-  const { prompt, imageBase64, type } = req.body;
-  if (!prompt || !imageBase64 || !type) {
-    res.status(400);
-    throw new Error('Thiếu dữ liệu cần thiết.');
-  }
-
-  const userId = req.user._id;
-
-  // Tạm thời mock AI ảnh trả về
-  const generatedUrl = `https://cdn.vonggioi.com/generated/${Date.now()}.jpg`;
-
-  const update = type === 'cover'
-    ? { cover: generatedUrl }
-    : { avatarFull: generatedUrl };
-
-  const updatedCharacter = await Character.findOneAndUpdate(
-    { userId },
-    { $set: update },
-    { new: true }
-  );
-
-  if (!updatedCharacter) {
-    res.status(404);
-    throw new Error('Không tìm thấy nhân vật.');
-  }
-
-  res.json({ success: true, imageUrl: generatedUrl });
-});
-
-module.exports = {
-  getCharacterProfile,
-  updateCharacterProfile,
-  renderCharacterImage
-};
+module.exports = { getCharacterProfile, updateCharacterProfile };
