4d54d23647ce25561fd84116a1ac78c10598b4bf
Ant <thanhpc.luxuryhome@gmail.com>
2025-05-15 11:54:39 +0700

    up

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use('/tuvi', tuviRoutes);
 app.use('/worlds', worldRoutes);
 app.use('/factions', factionRoutes);
 app.use('/sociallink', socialLinkRoutes);
-app.use('/ai', require('./routes/ai.route'));
 
 // Kết nối MongoDB
 mongoose.connect(process.env.MONGO_URI)
